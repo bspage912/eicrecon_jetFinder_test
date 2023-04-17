86886640eec4cb51c6b733cc400d244b3832ea66
bspage912 <bspage912@gmail.com>
2023-04-17 16:36:18 -0400

    Added jets from generated particles corresponding to what is in ReconstructedJets to do faithful reco/truth comparisions. Added code to find duplicate tracks in jets from ReconstructedParticles.

--- jetComparison.cxx
+++ jetComparison.cxx
@@ -45,12 +45,15 @@ int main(int argc, char* argv[]) {
   TTreeReaderArray<float> recoMomZ = {tree_reader, "ReconstructedJets.momentum.z"};
   TTreeReaderArray<float> recoM = {tree_reader, "ReconstructedJets.mass"};
 
-  //TTreeReaderArray<int> recoType = {tree_reader, "GeneratedJets.type"};
-  //TTreeReaderArray<float> recoNRG = {tree_reader, "GeneratedJets.energy"};
-  //TTreeReaderArray<int> recoPDG = {tree_reader, "GeneratedJets.PDG"};
-  //TTreeReaderArray<float> recoMomX = {tree_reader, "GeneratedJets.momentum.x"};
-  //TTreeReaderArray<float> recoMomY = {tree_reader, "GeneratedJets.momentum.y"};
-  //TTreeReaderArray<float> recoMomZ = {tree_reader, "GeneratedJets.momentum.z"};
+  /*
+  TTreeReaderArray<int> recoType = {tree_reader, "GeneratedJets.type"};
+  TTreeReaderArray<float> recoNRG = {tree_reader, "GeneratedJets.energy"};
+  TTreeReaderArray<int> recoPDG = {tree_reader, "GeneratedJets.PDG"};
+  TTreeReaderArray<float> recoMomX = {tree_reader, "GeneratedJets.momentum.x"};
+  TTreeReaderArray<float> recoMomY = {tree_reader, "GeneratedJets.momentum.y"};
+  TTreeReaderArray<float> recoMomZ = {tree_reader, "GeneratedJets.momentum.z"};
+  TTreeReaderArray<float> recoM = {tree_reader, "GeneratedJets.mass"};
+  */
   
   // MC
   TTreeReaderArray<int> mcGenStat = {tree_reader, "MCParticles.generatorStatus"};
@@ -93,17 +96,39 @@ int main(int argc, char* argv[]) {
   TH2D *localVsRecoJetEtaHist = new TH2D("localVsRecoJetEta","",1000,-5.,5.,1000,-5.,5.);
   TH2D *localVsRecoJetPhiHist = new TH2D("localVsRecoJetPhi","",500,-TMath::Pi(),TMath::Pi(),500,-TMath::Pi(),TMath::Pi());
 
+  TH1D *recoPartMassHist = new TH1D("recoPartMass","",10000,0.,1.);
+
+  TH1D *localPartCombDRHist = new TH1D("localPartCombDR","",1000,0.,10.);
+
+  // Comparison between Local Generated and Reco Jets
+  TH1D *localGenRecoDetlaRHist = new TH1D("localGenRecoDetlaR","",500,0.,5.);
+
+  TH2D *localGenVsRecoJetEHist = new TH2D("localGenVsRecoJetE","",300,0.,300.,300,0.,300.);
+  TH2D *localGenVsRecoJetEtaHist = new TH2D("localGenVsRecoJetEta","",100,-5.,5.,100,-5.,5.);
+  TH2D *localGenVsRecoJetPhiHist = new TH2D("localGenVsRecoJetPhi","",100,-TMath::Pi(),TMath::Pi(),500,-TMath::Pi(),TMath::Pi());
+
+  TH1D *localGenRecoJetEDiffHist = new TH1D("localGenRecoJetEDiff","",200,-10.,10.);
+  TH2D *localGenRecoJetEDiffVsRecoEHist = new TH2D("localGenRecoJetEDiffVsRecoE","",300,0.,300.,200,-10.,10.);
+  TH1D *localGenRecoJetEDiffDupTrackHist = new TH1D("localGenRecoJetEDiffDupTrack","",200,-10.,10.);
+  TH1D *localGenRecoJetEDiffNoDupTrackHist = new TH1D("localGenRecoJetEDiffNoDupTrack","",200,-10.,10.);
+
+  TH2D *localGenRecoJetEDiffVsDeltaRHist = new TH2D("localGenRecoJetEDiffVsDeltaR","",500,0.,5.,200,-10.,10.);
+
+  TH1D *localGenPartCombDRHist = new TH1D("localGenPartCombDR","",1000,0.,10.);
+
   
   // Begin Loop Over Events
   int NEVENTS = 0;
   int TOTPARTS = 0;
   int MISMATCHJETE = 0;
+  vector<int> dupTrackEvents;
   while(tree_reader.Next()) {
 
     if(NEVENTS%10000 == 0) cout << "Events Processed: " << NEVENTS << endl;
 
     // Reconstructed Particles for Clustering
     vector<PseudoJet> particles;
+    vector<PseudoJet> particlesGen;
 
     int numJets = 0;
     cout << "Event " << NEVENTS << " " << recoType.GetSize() << endl;
@@ -132,7 +157,7 @@ int main(int argc, char* argv[]) {
 		    local.SetPxPyPzE(recoMomX[j],recoMomY[j],recoMomZ[j],recoNRG[j]);
 		    totVec += local;
 
-		    //cout << "    Particle " << j << " Energy = " << recoNRG[j] << " Eta = " << local.PseudoRapidity() << " Phi = " << local.Phi() << " Mass = " << recoM[j] << endl;
+		    cout << "    Particle " << j << " Energy = " << recoNRG[j] << " Eta = " << local.PseudoRapidity() << " Phi = " << local.Phi() << " Mass = " << recoM[j] << " Pt = " << local.Perp() << endl;
 
 		    recoJetPartEvsEtaHist->Fill(local.PseudoRapidity(),recoNRG[j]);
 		    recoJetPartPhiVsEtaHist->Fill(local.PseudoRapidity(),local.Phi());
@@ -164,26 +189,34 @@ int main(int argc, char* argv[]) {
       {
 	TVector3 partMom(recoPartMomX[i],recoPartMomY[i],recoPartMomZ[i]);
 	double E = std::sqrt(recoPartMomX[i]*recoPartMomX[i]+recoPartMomY[i]*recoPartMomY[i]+recoPartMomZ[i]*recoPartMomZ[i]+recoPartM[i]*recoPartM[i]);
-	
-	//cout << "Reco Particle " << i << " Energy = " << E << " Eta = " << partMom.PseudoRapidity() << " Phi = " << partMom.Phi() << " Mass = " << recoPartM[i] << " Pt = " << partMom.Perp() << endl;
 
-	if(1) //partMom.Perp() > 0.2 && partMom.Perp() < 100.0)
+	recoPartMassHist->Fill(recoPartM[i]);
+	
+	cout << "Reco Particle " << i << " Energy = " << E << " Eta = " << partMom.PseudoRapidity() << " Phi = " << partMom.Phi() << " Mass = " << recoPartM[i] << " Pt = " << partMom.Perp() << endl;
+
+	if(partMom.Perp() > 0.2 && partMom.Perp() < 100.0)
 	  {
 	    particles.push_back( PseudoJet(partMom.Px(),partMom.Py(),partMom.Pz(),E) );
 	  }
       }
-    //cout << endl;
+    cout << endl;
     for(unsigned int i=0; i<mcMomX.GetSize(); i++)
       {
 	if(mcGenStat[i] == 1)
 	  {
-	    TVector3 jetMom(mcMomX[i],mcMomY[i],mcMomZ[i]);
+	    TVector3 partMom(mcMomX[i],mcMomY[i],mcMomZ[i]);
 	    double E = std::sqrt(mcMomX[i]*mcMomX[i] + mcMomY[i]*mcMomY[i] + mcMomZ[i]*mcMomZ[i] + mcM[i]*mcM[i]);
+	    int id = std::abs(pdg[i]);
 	    
-	    //cout << "Particle " << i << " Energy = " << E << " Eta = " << jetMom.PseudoRapidity() << " Phi = " << jetMom.Phi() << " PDG = " << pdg[i] << endl;
+	    cout << "Particle " << i << " Energy = " << E << " Eta = " << partMom.PseudoRapidity() << " Phi = " << partMom.Phi() << " PDG = " << pdg[i] << endl;
+
+	    if(id==11 || id==13 || id==211 || id==321 || id==2212)
+	      {
+		particlesGen.push_back( PseudoJet(partMom.Px(),partMom.Py(),partMom.Pz(),E) );
+	      }
 	  }
       }
-    //cout << endl;
+    cout << endl;
 
 
     // Find Jets from Reconstructed Particles
@@ -191,16 +224,18 @@ int main(int argc, char* argv[]) {
     JetDefinition jet_def_akt(antikt_algorithm,1.0);
 
     // Cluster in Beam Frame
-    ClusterSequence cs_akt_all_beam(particles, jet_def_akt);
+    ClusterSequence cs_akt_reco(particles, jet_def_akt);
+    ClusterSequence cs_akt_gen(particlesGen, jet_def_akt);
 
     // Set Min Jet Pt
     //double ptmin = 1.0;
 
     // Get Jets
-    vector<PseudoJet> jets_akt_all_beam = sorted_by_pt(cs_akt_all_beam.inclusive_jets());
+    vector<PseudoJet> jets_akt_reco = sorted_by_pt(cs_akt_reco.inclusive_jets());
+    vector<PseudoJet> jets_akt_gen = sorted_by_pt(cs_akt_gen.inclusive_jets());
 
     // Compare to Jets from EICrecon
-    numLocalVsRecoJetsHist->Fill(numJets,jets_akt_all_beam.size());
+    numLocalVsRecoJetsHist->Fill(numJets,jets_akt_reco.size());
 
     // Loop Over Reco Jets
     for(unsigned int i=0; i<recoType.GetSize(); i++)
@@ -223,12 +258,13 @@ int main(int argc, char* argv[]) {
 		  }
 	      }
 
+	    // Match reco Jet to Local Jet
 	    double minDist = 10000.0;
 	    int matchIndex = -1;
-	    for(unsigned int jn=0; jn<jets_akt_all_beam.size(); jn++)
+	    for(unsigned int jn=0; jn<jets_akt_reco.size(); jn++)
 	      {
-		double dEta = jetMom.PseudoRapidity() - jets_akt_all_beam[jn].eta();
-		double dPhi = TVector2::Phi_mpi_pi(jetMom.Phi() - jets_akt_all_beam[jn].phi());
+		double dEta = jetMom.PseudoRapidity() - jets_akt_reco[jn].eta();
+		double dPhi = TVector2::Phi_mpi_pi(jetMom.Phi() - jets_akt_reco[jn].phi());
 		double dR = std::sqrt(dEta*dEta + dPhi*dPhi);
 
 		if(dR < minDist)
@@ -238,31 +274,107 @@ int main(int argc, char* argv[]) {
 		  }
 	      }
 
+	    // Match reco jet to local Gen jet
+	    double minDistGen = 10000.0;
+	    int matchIndexGen = -1;
+	    for(unsigned int jn=0; jn<jets_akt_gen.size(); jn++)
+	      {
+		double dEta = jetMom.PseudoRapidity() - jets_akt_gen[jn].eta();
+		double dPhi = TVector2::Phi_mpi_pi(jetMom.Phi() - jets_akt_gen[jn].phi());
+		double dR = std::sqrt(dEta*dEta + dPhi*dPhi);
+
+		if(dR < minDistGen)
+		  {
+		    minDistGen = dR;
+		    matchIndexGen = jn;
+		  }
+	      }
+
+	    // Tag Local Reco Jets With Duplicate Tracks
+	    bool dupTrack = false;
+
+	    // Look at Matched Reco / Local jets
 	    if(matchIndex > -1)
 	      {
-		localVsRecoJetPtHist->Fill(jetMom.Perp(),jets_akt_all_beam[matchIndex].pt());
-		localVsRecoJetEHist->Fill(recoNRG[i],jets_akt_all_beam[matchIndex].e());
-		localVsRecoJetEtaHist->Fill(jetMom.PseudoRapidity(),jets_akt_all_beam[matchIndex].eta());
-		localVsRecoJetPhiHist->Fill(jetMom.Phi(),jets_akt_all_beam[matchIndex].phi());
+		localVsRecoJetPtHist->Fill(jetMom.Perp(),jets_akt_reco[matchIndex].pt());
+		localVsRecoJetEHist->Fill(recoNRG[i],jets_akt_reco[matchIndex].e());
+		localVsRecoJetEtaHist->Fill(jetMom.PseudoRapidity(),jets_akt_reco[matchIndex].eta());
+		localVsRecoJetPhiHist->Fill(TVector2::Phi_mpi_pi(jetMom.Phi()),TVector2::Phi_mpi_pi(jets_akt_reco[matchIndex].phi()));
 
 		// Compare Constituents
-		vector<PseudoJet> cons = jets_akt_all_beam[matchIndex].constituents();
+		vector<PseudoJet> cons = jets_akt_reco[matchIndex].constituents();
 
 		int numLocalJetPart = 0;
 		for(unsigned int jc=0; jc<cons.size(); jc++)
 		  {
 		    if(cons[jc].pt() > 0.2 && cons[jc].pt() < 100.0)
 		      numLocalJetPart++;
+
+		    if(jc < cons.size() -1)
+		      {
+			for(unsigned int jc1 = jc+1; jc1<cons.size(); jc1++)
+			  {
+			    double dEta = cons[jc].eta() - cons[jc1].eta();
+			    double dPhi = TVector2::Phi_mpi_pi(cons[jc].phi() - cons[jc1].phi());
+			    double dR = std::sqrt(dEta*dEta + dPhi*dPhi);
+
+			    localPartCombDRHist->Fill(dR);
+
+			    if(dR < 0.05) 
+			      {
+				dupTrack = true;
+				dupTrackEvents.push_back(NEVENTS);
+			      }
+			  }
+		      }
 		  }
 
 		numLocalVsRecoJetPartsHist->Fill(numParts,numLocalJetPart);
 
-		if(std::abs(recoNRG[i] - jets_akt_all_beam[matchIndex].e()) > 10e-6) 
+		if(std::abs(recoNRG[i] - jets_akt_reco[matchIndex].e()) > 10e-6) 
 		  {
-		    cout << "Event " << NEVENTS << " Jet " << i << " has Mismatched Energy = " << recoNRG[i] - jets_akt_all_beam[matchIndex].e() << endl;
+		    //cout << "Event " << NEVENTS << " Jet " << i << " has Mismatched Energy = " << recoNRG[i] - jets_akt_all_beam[matchIndex].e() << endl;
 		    MISMATCHJETE++;
 		  }
 	      }
+
+	    // Look at matched local Gen vs Reco jets
+	    localGenRecoDetlaRHist->Fill(minDistGen);
+	    if(matchIndexGen > -1 && minDistGen < 0.275)
+	      {
+		localGenVsRecoJetEHist->Fill(recoNRG[i],jets_akt_gen[matchIndexGen].e());
+		localGenVsRecoJetEtaHist->Fill(jetMom.PseudoRapidity(),jets_akt_gen[matchIndexGen].eta());
+		localGenVsRecoJetPhiHist->Fill(TVector2::Phi_mpi_pi(jetMom.Phi()),TVector2::Phi_mpi_pi(jets_akt_gen[matchIndexGen].phi()));
+
+		localGenRecoJetEDiffHist->Fill((recoNRG[i]-jets_akt_gen[matchIndexGen].e())/jets_akt_gen[matchIndexGen].e());
+		localGenRecoJetEDiffVsRecoEHist->Fill(recoNRG[i],(recoNRG[i]-jets_akt_gen[matchIndexGen].e())/jets_akt_gen[matchIndexGen].e());
+		if(dupTrack) localGenRecoJetEDiffDupTrackHist->Fill((recoNRG[i]-jets_akt_gen[matchIndexGen].e())/jets_akt_gen[matchIndexGen].e());
+		if(!dupTrack) localGenRecoJetEDiffNoDupTrackHist->Fill((recoNRG[i]-jets_akt_gen[matchIndexGen].e())/jets_akt_gen[matchIndexGen].e());
+
+		localGenRecoJetEDiffVsDeltaRHist->Fill(minDistGen,(recoNRG[i]-jets_akt_gen[matchIndexGen].e())/jets_akt_gen[matchIndexGen].e());
+
+		cout << "Gen Jet (Reco = " << recoPDG[i] << ") Energy = " << jets_akt_gen[matchIndexGen].e() << " Eta = " << jets_akt_gen[matchIndexGen].eta() << " Phi = " << TVector2::Phi_mpi_pi(jets_akt_gen[matchIndexGen].phi()) << " Delta R = " << minDistGen << " Delta E/E = " << (recoNRG[i]-jets_akt_gen[matchIndexGen].e())/jets_akt_gen[matchIndexGen].e() << endl;
+
+		// Compare Constituents
+		vector<PseudoJet> cons = jets_akt_gen[matchIndexGen].constituents();
+		for(unsigned int jc=0; jc<cons.size(); jc++)
+		  {
+		    cout << "    Particle " << jc << " Energy = " << cons[jc].e() << " Eta = " << cons[jc].eta() << " Phi = " << TVector2::Phi_mpi_pi(cons[jc].phi()) << " Pt = " << cons[jc].pt() << endl;
+
+		    if(jc < cons.size() -1)
+		      {
+			for(unsigned int jc1 = jc+1; jc1<cons.size(); jc1++)
+			  {
+			    double dEta = cons[jc].eta() - cons[jc1].eta();
+			    double dPhi = TVector2::Phi_mpi_pi(cons[jc].phi() - cons[jc1].phi());
+			    double dR = std::sqrt(dEta*dEta + dPhi*dPhi);
+
+			    localGenPartCombDRHist->Fill(dR);
+			  }
+		      }
+		  }
+	      }
+	    cout << endl;
 	  }
       }
 
@@ -271,6 +383,11 @@ int main(int argc, char* argv[]) {
 
   cout << "Total Particles = " << TOTPARTS << endl;
   cout << "Total Reco/Local Jet Pairs with Mismatched Energy = " << MISMATCHJETE << endl;
+  cout << "Events with Duplicate Tracks" << endl;
+  for(unsigned int i=0; i<dupTrackEvents.size(); i++)
+    {
+      cout << dupTrackEvents[i] << endl;
+    }
 
   //mcEta->Write();
   //mcNRG->Write();
